356bcb79fdc475e8f52277bc3cf2752c6edad9d0
P334257 <marcus.krutwig@porsche.de>
2025-01-18 13:56:53 +0100

    changes and updates do .db and classifier.pkl after rerunning the code as a final test.

--- app/run.py
+++ app/run.py
@@ -46,11 +46,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///data/DisasterResponse.db')
 df = pd.read_sql_table('TAB_DR', engine)
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
